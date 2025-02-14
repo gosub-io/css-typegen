1e2ac85fdd3f2d90c8c81e0bea01565343363741
Shark <sharktheone@proton.me>
2025-02-14 09:27:30 +0100

    basic things, ensue multipliers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod component;
+mod multiplier;
+
+use gosub_css3::matcher::property_definitions::get_css_definitions;
+use proc_macro2::Span;
+use syn::ItemEnum;
+use syn::punctuated::Punctuated;
+use crate::component::generate_component;
+
 fn main() {
-    println!("Hello, world!");
+
+    let defs = get_css_definitions();
+
+    let cubic = defs.syntax.get("display-outside").unwrap();
+
+
+    let mut ty = new_enum("DisplayOutside");
+    
+    generate_component(&mut ty, &cubic.syntax.components[0])
 }
+
+fn new_enum(name: &str) -> ItemEnum {
+    ItemEnum {
+        attrs: vec![],
+        vis: syn::Visibility::Inherited,
+        enum_token: Default::default(),
+        ident: syn::Ident::new(name, Span::call_site()),
+        generics: Default::default(),
+        brace_token: Default::default(),
+        variants: Punctuated::new()
+    }
+}
+
+
+
---
+++ src/multiplier.rs
@@ -0,0 +1,60 @@
+
+
+
+pub enum MultiplierError {
+    NotEnoughItems,
+    TooManyItems,
+}
+
+
+pub struct ZeroOrMore<T>(Vec<T>);
+pub struct OneOrMore<T>(Vec<T>);
+pub struct Between<const L: usize, const U: usize, T>(Vec<T>);
+// pub struct AtLeastOne //TODO
+pub struct CommaSeparatedRepeat<const L: usize, const U: usize, T>(Vec<T>);
+
+
+impl<T> ZeroOrMore<T> {
+    pub fn new(items: Vec<T>) -> Self {
+        Self(items)
+    }
+}
+
+impl<T> OneOrMore<T> {
+    pub fn new(items: Vec<T>) -> Result<Self, MultiplierError> {
+        if items.is_empty() {
+            return Err(MultiplierError::NotEnoughItems)
+        }
+        
+        Ok(Self(items))
+    }
+}
+
+
+impl<const L: usize, const U: usize, T> Between<L, U, T> {
+    pub fn new(items: Vec<T>) -> Result<Self, MultiplierError> {
+        if items.len() < L {
+            return Err(MultiplierError::NotEnoughItems)
+        }
+        
+        if items.len() > U {
+            return Err(MultiplierError::TooManyItems)
+        }
+        
+        Ok(Self(items))
+    }
+}
+
+impl<const L: usize, const U: usize, T> CommaSeparatedRepeat<L, U, T> {
+    pub fn new(items: Vec<T>) -> Result<Self, MultiplierError> {
+        if items.len() < L {
+            return Err(MultiplierError::NotEnoughItems)
+        }
+        
+        if items.len() > U {
+            return Err(MultiplierError::TooManyItems)
+        }
+        
+        Ok(Self(items))
+    }
+}
